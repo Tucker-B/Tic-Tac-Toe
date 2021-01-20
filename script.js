1284e3354477b3a335320d7b05bd917b4b00c0a3
Tucker Barton <hisashiburix@gmail.com>
2021-01-20 18:53:31 -0500

    Stylize h1, p, etc. Create basic gameboard in html via elements.

---
+++ script.js
@@ -0,0 +1,20 @@
+
+
+/*
+    1. Store gameboard as an array inside of a Gameboard object
+    2. Player objects
+    3. Game mechanics object
+    // Side note: use as few global names as possible
+    //            modules: only need one of it, so "gameBoard", "display"
+                  factory: need multiples, "players"
+*/
+
+
+(function gameBoard() {
+    
+    
+
+
+
+})();
+
