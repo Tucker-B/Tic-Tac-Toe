843b3e811f8d67e5379296059153d152dd08f0c5
Tucker Barton <hisashiburix@gmail.com>
2021-01-21 10:59:00 -0500

    Populate board[] such that it is a 3x3 array.

--- script.js
+++ script.js
@@ -11,10 +11,19 @@
 
 
 (function gameBoard() {
-    
-    
-
-
-
+    let board = [];
+    // Populates board array with 'B' ([B]lank spaces)
+    for (let i = 0; i < 3; i++) {
+        board[i] = [];
+        // let rowString = "";
+        for (let j = 0; j < 3; j++) {
+            board[i][j] = 'B';
+            // rowString += board[i][j];
+            // const container = document.querySelector(`#row-${i+1}-span-${j+1}`);
+            // container.textContent = board[i][j];
+            
+        }
+        // console.log(rowString);
+    }
 })();
 
