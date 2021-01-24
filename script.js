1acfe7d9f99002dbc819cebc12c800f29d18a88e
Tucker Barton <hisashiburix@gmail.com>
2021-01-24 09:59:35 -0500

    Fix bug where spanElement in play() in script.js is undefined.

--- script.js
+++ script.js
@@ -16,14 +16,15 @@ function Player(name, playSymbol) {
 
 // Changes span elements for webpage, deletes blank space in blankSpaceOnBoard[]
 function play(blankSpaceOnBoard, gameSymbol, xInput, yInput) {
-    // 1. Change element in document
-    let spanElement = document.querySelector(`#row-${yInput+1}-span-${xInput+1}`);
-    spanElement.textContent = board[yInput][xInput]; 
 
-    // 2. Change spot in board[] to no longer be 'B'
+    // 1. Change spot in board[] to no longer be 'B'
     yInput = parseInt(yInput);
     xInput = parseInt(xInput);
     board[yInput][xInput] = gameSymbol;
+
+    // 2. Change element in document
+    let spanElement = document.querySelector(`#row-${yInput+1}-span-${xInput+1}`);
+    spanElement.textContent = gameSymbol;
 
     // 3. access blankSpaceOnBoard[] and delete that specific index
     blankSpaceOnBoard[yInput].splice(xInput, 1);
